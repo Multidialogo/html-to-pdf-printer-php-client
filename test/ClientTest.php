ae894f3192ecf5a9cc6b7550ad64d4b74d9cab13
Emidio Cruciani <cruciani@multidialogo.it>
2024-10-30 09:43:05 +0100

    MD/REWAMCB0102 | fixed tests

--- test/ClientTest.php
+++ test/ClientTest.php
@@ -1,9 +1,8 @@
 <?php
 
-namespace MultiDialogo\HtmlToPdfPrinterPhpClient\Test;
-
 use GuzzleHttp\Client as GuzzleClient;
 use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use MultiDialogo\HtmlToPdfPrinterPhpClient\Client;
 use PHPUnit_Framework_TestCase;
@@ -13,21 +12,11 @@ class ClientTest extends PHPUnit_Framework_TestCase
     private $guzzleClientMock;
     private $client;
 
-    protected function setUp()
-    {
-        $this->guzzleClientMock = $this->getMockBuilder(GuzzleClient::class) // Updated mock creation for PHPUnit 4.8
-        ->disableOriginalConstructor()
-            ->getMock();
-
-        $this->client = new Client('https://api.example.com');
-        $this->client->setClient($this->guzzleClientMock);
-    }
-
     public function testGetHtmlAsPdfStreamSuccess()
     {
         $htmlBody = '<h1>Hello, World!</h1>';
         $callerService = 'TestService';
-        $filePath = '/path/to/valid/file.pdf';
+        $filePath = __DIR__ . '/resources/empty.pdf';
 
         $this->guzzleClientMock->method('post')->willReturn(new Response(200, [], json_encode([
             'data' => [
@@ -43,15 +32,13 @@ class ClientTest extends PHPUnit_Framework_TestCase
 
         $this->assertTrue(is_resource($stream)); // Use is_resource() instead of assertIsResource()
         fclose($stream);
-
-        unlink($filePath);
     }
 
     public function testGetHtmlAsPdfStreamMissingFile()
     {
         $htmlBody = '<h1>Hello, World!</h1>';
         $callerService = 'TestService';
-        $filePath = '/path/to/missing/file.pdf';
+        $filePath = __DIR__ . '/path/to/missing/file.pdf';
 
         // Mock the response from the Guzzle client
         $this->guzzleClientMock->method('post')->willReturn(new Response(200, [], json_encode([
@@ -78,5 +65,13 @@ class ClientTest extends PHPUnit_Framework_TestCase
         $this->setExpectedException('RuntimeException', "Error during request"); // Change to setExpectedException
 
         $this->client->getHtmlAsPdfStream($callerService, $htmlBody);
+    }
+
+    protected function setUp(): void
+    {
+        $this->guzzleClientMock = $this->createMock(GuzzleClient::class);
+
+        $this->client = new Client('https://api.example.com');
+        $this->client->setClient($this->guzzleClientMock);
     }
 }
